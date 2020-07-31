fa8dd9fff06a93bd1503835c2413f72811b78127
André Adriano <andre@dev-next.com>
2020-07-31 17:29:48 -0300

    :sparkles: Create base for TFrame component

---
+++ components/TFrame/TFrame.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="frame">
+    <div class="frame__title">
+      {{ props.title }}
+    </div>
+    <div class="frame__body">
+      // TODO
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import { Frame } from '@/services/ootz'
+
+export default Vue.extend({
+  props: {
+    frame: {
+      type: Object,
+      required: true
+    } as PropOptions<Frame>
+  }
+})
+</script>
