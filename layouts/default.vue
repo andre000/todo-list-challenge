8e0e8eec0b5e49edc6f5eda498c25068b2a694d8
André <andre@dev-next.com>
2020-07-31 12:20:54 -0300

    :sparkles: Create main template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="page">
+    <header class="page__header">
+      Header
+    </header>
+    <main class="page__main">
+      <Nuxt />
+    </main>
+    <footer class="page__footer">
+      Footer
+    </footer>
   </div>
 </template>
+
+<style lang="scss">
+@import url('@/assets/scss/main.scss');
+
+.page {
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  min-height: 100vh;
+}
+</style>
