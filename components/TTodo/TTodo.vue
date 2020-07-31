d36dd3009a31be2ce51de058bfd731293188bf61
André Adriano <andre@dev-next.com>
2020-07-31 17:30:51 -0300

    :sparkles: Create base from TTodo component

---
+++ components/TTodo/TTodo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <div class="card__title">
+      {{ todo.title }}
+    </div>
+    <div class="card__body">
+      {{ todo.body }}
+    </div>
+    <div class="card__actions">
+      <i class="card__action__edit" />
+      <i class="card__action__close" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import { Todo } from '@/services/ootz'
+
+export default Vue.extend({
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    } as PropOptions<Todo>
+  }
+})
+</script>
