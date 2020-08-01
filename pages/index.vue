dbef194927f22bdc86e7a14899825cfd8ca7c55e
Andre <andre@dev-next.com>
2020-08-01 20:01:57 -0300

    ✨ Add computed data to index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="container">
+    <t-frame-container :frames="frames" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  computed: {
+    frames () {
+      return this.$store.getters.fullFrames
+    }
+  }
+})
 </script>
+
+<style lang="scss" scoped>
+
+</style>
