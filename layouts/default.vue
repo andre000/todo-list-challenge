041dd165954588b567bc5f6ae3b739a3cdb4c719
Andre <andre@dev-next.com>
2020-08-01 20:02:19 -0300

    💄 Add style to layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="page">
     <header class="page__header">
-      Header
+      <h1>
+        Header
+      </h1>
     </header>
     <main class="page__main">
       <Nuxt />
     </main>
     <footer class="page__footer">
-      Footer
+      <a href="http://github.com/andre000">@andre000</a>
     </footer>
   </div>
 </template>
@@ -18,6 +20,22 @@
 .page {
   display: grid;
   grid-template-rows: auto 1fr auto;
-  min-height: calc(100vh - 1px);
+  min-height: calc(100vh - 1px - 10vh);
+  padding: 5vh 15vw;
+
+  &__footer {
+    text-align: center;
+    a {
+      text-decoration: none;
+      font-size: 11px;
+      color: #888a;
+      &:hover {
+        text-decoration: underline;
+      }
+      .dark & {
+        color: #fff5;
+      }
+    }
+  }
 }
 </style>
