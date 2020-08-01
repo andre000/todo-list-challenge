3ecfc2a3f0e4840ef8521f40b13c3ea3ced85e87
Andre <andre@dev-next.com>
2020-08-01 20:02:42 -0300

    💄 Add style to TFrameContainer component

--- components/TFrameContainer/TFrameContainer.vue
+++ components/TFrameContainer/TFrameContainer.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="frame__container">
-    <slot />
+    <t-frame v-for="frame in frames" :key="frame.id" :frame="frame" />
   </div>
 </template>
+
+<script lang="ts">
+import Vue, { PropOptions } from 'vue'
+import { Frame } from '@/services/ootz'
+
+export default Vue.extend({
+  props: {
+    frames: {
+      type: Array,
+      default: () => []
+    } as PropOptions<Array<Frame>>
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.frame__container {
+  display: flex;
+}
+</style>
