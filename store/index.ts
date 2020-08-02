6df346add68e78798cbd213d44e7982ad38098f1
Andre <andre@dev-next.com>
2020-08-02 00:55:52 -0300

    🐛 Fix mock frame structure

--- store/index.ts
+++ store/index.ts
@@ -1,47 +1,49 @@
-import { Frame, Todo } from '@/services/ootz'
+import { Store } from 'vuex'
+import api, { Frame, Todo } from '@/services/ootz'
 
 const mockFrame: Array<Frame> = [
   {
     id: 'abc1',
     title: 'Frame 1',
     created_at: '2020-01-01 12:50:01',
-    order: 1
+    order: 1,
+    todos: [
+      {
+        id: 'cdb1',
+        frame_id: 'abc1',
+        title: 'TODO',
+        description: 'Todo 1',
+        open: true,
+        order: 1,
+        created_at: '2020-01-01 12:50:01'
+      },
+      {
+        id: 'cdb2',
+        frame_id: 'abc1',
+        title: 'TODO',
+        description: 'Todo 2',
+        open: true,
+        order: 2,
+        created_at: '2020-01-01 12:50:01'
+      }
+    ]
   },
   {
     id: 'abc2',
     title: 'Frame 2',
     created_at: '2020-01-01 12:50:01',
-    order: 2
-  }
-]
-
-const mockTodo: Array<Todo> = [
-  {
-    id: 'cdb1',
-    frame_id: 'abc1',
-    title: 'TODO',
-    description: 'Todo 1',
-    open: true,
-    order: 1,
-    created_at: '2020-01-01 12:50:01'
-  },
-  {
-    id: 'cdb2',
-    frame_id: 'abc1',
-    title: 'TODO',
-    description: 'Todo 2',
-    open: true,
     order: 2,
-    created_at: '2020-01-01 12:50:01'
-  },
-  {
-    id: 'cdb3',
-    frame_id: 'abc2',
-    title: 'TODO',
-    description: 'Todo 3',
-    open: true,
-    order: 2,
-    created_at: '2020-01-01 12:50:01'
+    todos: [
+      {
+        id: 'cdb3',
+        frame_id: 'abc2',
+        title: 'TODO',
+        description: 'Todo 3',
+        open: true,
+        order: 2,
+        created_at: '2020-01-01 12:50:01'
+      }
+    ]
   }
 ]
 
@@ -55,11 +57,13 @@ export interface TodoState {
 }
 
 export const state = () => ({
-  frames: [...mockFrame],
-  todos: [...mockTodo]
+  frames: [...mockFrame]
 } as TodoState)
 
 export const mutations = {
+  UPDATE_FRAME_LIST (state: TodoState, frames: Array<Frame>) {
+    state.frames = frames
+  },
   ADD_FRAME (state: TodoState, frame: Frame) {
     state.frames.push(frame)
   },
@@ -70,18 +74,6 @@ export const mutations = {
   DELETE_FRAME (state: TodoState, frameId: String) {
     const index = state.frames.findIndex(f => f.id === frameId)
     state.frames.splice(index, 1)
-  },
-
-  ADD_TODO (state: TodoState, todo: Todo) {
-    state.todos.push(todo)
-  },
-  EDIT_TODO (state: TodoState, todo: Todo) {
-    const index = state.todos.findIndex(f => f.id === todo.id)
-    state.todos[index] = todo
-  },
-  DELETE_TODO (state: TodoState, todoId: String) {
-    const index = state.todos.findIndex(f => f.id === todoId)
-    state.todos.splice(index, 1)
   }
 }
 
@@ -91,19 +83,24 @@ export const getters = {
 
     return state.frames.map(f => ({
       ...f,
-      todos: state
-        .todos
+      todos: f.todos ? f.todos
         .filter(t => t.frame_id === f.id)
-        .sort(sortByOrder)
+        .sort(sortByOrder) : []
     })).sort(sortByOrder)
   }
 }
 
 export const actions = {
+  async getFrame () {},
+  async listFrame (store: Store<TodoState>) {
+    const { data: frames } = await api.frame.list()
+    store.commit('UPDATE_FRAME_LIST', frames)
+  },
   async addFrame () {},
   async editFrame () {},
   async deleteFrame () {},
 
+  async getTodo () {},
   async addTodo () {},
   async editTodo () {},
   async deleteTodo () {}
