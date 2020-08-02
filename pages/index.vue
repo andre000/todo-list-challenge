5772d9ad27c8b0c5f1483f1efa97dab00fe0e2b6
Andre <andre@dev-next.com>
2020-08-02 00:56:21 -0300

    ✨ Add fetch on index page

--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,19 @@
 import Vue from 'vue'
 
 export default Vue.extend({
+  async fetch () {
+    await this.$store.dispatch('listFrame')
+  },
+
   computed: {
     frames () {
       return this.$store.getters.fullFrames
+    }
+  },
+
+  methods: {
+    refresh () {
+      this.$fetch()
     }
   }
 })
