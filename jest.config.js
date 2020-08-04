a95e510b603f3521f9fd721dfc7675b31dc6cbd7
André Adriano <andre@dev-next.com>
2020-08-04 12:01:23 -0300

    :wrench: Config Jest on project

--- jest.config.js
+++ jest.config.js
@@ -14,10 +14,5 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
     '.*\\.(vue)$': 'vue-jest'
-  },
-  collectCoverage: true,
-  collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
-  ]
+  }
 }
